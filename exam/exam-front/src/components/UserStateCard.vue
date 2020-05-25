8ee176c4f1c452de973fff08613ab4f59ca48baa
Arne <arnesacnussem@gmail.com>
2020-05-25 16:06:33 +0800

    nginx config

---
+++ exam/exam-front/src/components/UserStateCard.vue
@@ -0,0 +1,36 @@
+<template>
+
+</template>
+
+<script lang='ts'>
+  import Vue, { PropType } from 'vue';
+  import { UserTrack, UserTrackInfo } from '~/generated/openapi';
+
+  export default Vue.extend({
+    name: 'UserStateCard',
+
+    props: {
+      user_track: {
+        type: Object as PropType<UserTrack>,
+        required: true,
+      },
+    },
+
+    computed: {
+      latestActive(): UserTrackInfo {
+        return this.timeline[this.timeline.length - 1];
+      },
+    },
+
+    data() {
+      return {
+        userName: this.user_track.user,
+        timeline: this.user_track.timeline,
+      };
+    },
+  });
+</script>
+
+<style scoped>
+
+</style>
